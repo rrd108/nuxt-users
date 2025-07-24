50369a9a34c0ffa8e16e752a0112b63b7fe90255
rrd108 <rrd@webmania.cc>
2025-07-24 14:50:17 +0200

    test: cleanup added

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,10 +1,17 @@
 import { fileURLToPath } from 'node:url'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterEach } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils/e2e'
+import path from 'node:path'
+import fs from 'node:fs'
 
 describe('ssr', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
+  })
+
+  afterEach(async () => {
+    // remove the test db file
+    await fs.promises.unlink(path.join(process.cwd(), '_basic-test'))
   })
 
   it('renders the index page', async () => {
