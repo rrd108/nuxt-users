b4daec661a2a648aafd3b5ee4b4b4a6653156515
rrd108 <rrd@webmania.cc>
2025-08-02 18:34:06 +0200

    chore: bump cli version number

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -12,7 +12,7 @@ const main = defineCommand({
   meta: {
     name: 'nuxt-users',
     description: 'CLI for Nuxt Users Module - Manage users, migrations, and database operations',
-    version: '1.6.5'
+    version: '1.6.6'
   },
   subCommands: {
     migrate,
