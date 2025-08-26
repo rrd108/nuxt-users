a0a371c21a41bac990c1ca73e6d30eac94d6a718
rrd108 <rrd@webmania.cc>
2025-08-26 12:57:23 +0200

    fix: add missing endpoint

--- src/runtime/constants.ts
+++ src/runtime/constants.ts
@@ -2,4 +2,4 @@
 export const NO_AUTH_PATHS = ['/login', '/reset-password']
 
 // API routes that don't require authentication (will be prefixed with apiBasePath)
-export const NO_AUTH_API_PATHS = ['/session', '/password/forgot', '/password/reset']
+export const NO_AUTH_API_PATHS = ['/session', '/password/forgot', '/password/reset', '/me']
